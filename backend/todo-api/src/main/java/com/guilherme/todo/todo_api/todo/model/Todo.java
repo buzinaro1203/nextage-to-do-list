440577d3ad03e2bda1a46f01ce1f77ca3f5f3b27
buzinaro1203 <guilhermebuzinaro212@gmail.com>
2025-08-05 17:53:22 -0300

    :art: feat: enhance Todo model with timestamps and lifecycle methods for creation and updates

--- backend/todo-api/src/main/java/com/guilherme/todo/todo_api/todo/model/Todo.java
+++ backend/todo-api/src/main/java/com/guilherme/todo/todo_api/todo/model/Todo.java
@@ -1,8 +1,111 @@
 package com.guilherme.todo.todo_api.todo.model;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
 
 @Entity
+@Table(name = "todos")
 public class Todo {
+  // Fields
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  @Column(nullable = false, length = 50)
+  private String title;
+  @Column(length = 255)
+  private String description;
+
+  @Column(nullable = false)
+  private boolean completed = false;
+  @Column(nullable = false, updatable = false)
+  private LocalDateTime createdAt;
+  private LocalDateTime updatedAt;
+  private LocalDateTime completedAt;
+
+  // Default constructor
+  public Todo() {
+  }
+
+  // On create and update methods
+  @PrePersist
+  public void onCreate() {
+    this.createdAt = LocalDateTime.now();
+  }
+
+  @PreUpdate
+  public void onUpdate() {
+    this.updatedAt = LocalDateTime.now();
+    if (this.completed && this.completedAt == null) {
+      this.completedAt = LocalDateTime.now();
+    }
+    if (!this.completed && this.completedAt != null) { // Reset completedAt if not completed
+      this.completedAt = null;
+    }
+  }
+
+  // Getters and Setters
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public boolean isCompleted() {
+    return completed;
+  }
+
+  public void setCompleted(boolean completed) {
+    this.completed = completed;
+  }
+
+  public LocalDateTime getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(LocalDateTime createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  public LocalDateTime getUpdatedAt() {
+    return updatedAt;
+  }
+
+  public void setUpdatedAt(LocalDateTime updatedAt) {
+    this.updatedAt = updatedAt;
+  }
+
+  public LocalDateTime getCompletedAt() {
+    return completedAt;
+  }
+
+  public void setCompletedAt(LocalDateTime completedAt) {
+    this.completedAt = completedAt;
+  }
 
 }
