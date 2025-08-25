a28d7731af4e06e900760db7fa5f6e0cd42abf6a
buzinaro1203 <guilhermebuzinaro212@gmail.com>
2025-08-25 15:41:49 -0300

    Tryng to fix routes in vercel

--- frontend/todo-list-project/src/App.jsx
+++ frontend/todo-list-project/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage.jsx";
 import HomePage from "./pages/HomePage.jsx";
 
