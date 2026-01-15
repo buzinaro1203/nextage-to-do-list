34a719050c17d697a13d82d1d56cb74550271cd6
buzinaro1203 <guilhermebuzinaro212@gmail.com>
2026-01-15 12:24:05 -0300

    fix: update API endpoints and improve environment variable usage

--- frontend/todo-list-project/src/api/api.js
+++ frontend/todo-list-project/src/api/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
 
 export const createApi = (email, password) => {
+  // BUG: A função btoa não suporta caracteres especiais em algumas situações ou acentuações.
   const token = btoa(`${email}:${password}`);
   return axios.create({
     baseURL: BASE_URL,
@@ -14,7 +15,7 @@ export const createApi = (email, password) => {
 };
 
 export const registerUser = async (name, email, password) => {
-  const response = await axios.post(`${BASE_URL}/api/users/register`, {
+  const response = await axios.post(`${BASE_URL}/users/register`, {
     name,
     email,
     password,
@@ -27,28 +28,28 @@ export const registerUser = async (name, email, password) => {
 // ----------------------
 
 export const fetchTodos = async (api) => {
-  const response = await api.get("/api/todos");
+  const response = await api.get("/todos");
   console.log("Fetched todos:", response.data);
   return response.data;
 };
 
 export const createTodo = async (api, todo) => {
   console.log("Creating Todo", todo);
-  const response = await api.post("/api/todos", todo);
+  const response = await api.post("/todos", todo);
   return response.data;
 };
 
 export const alterTodo = async (api, id, todo) => {
-  const response = await api.put(`/api/todos/${id}`, todo);
+  const response = await api.put(`/todos/${id}`, todo);
   return response.data;
 };
 
 export const deleteTodo = async (api, id) => {
-  await api.delete(`/api/todos/${id}`);
+  await api.delete(`/todos/${id}`);
 };
 
 export const completeTodo = async (api, id, todo) => {
   const updatedTodo = { ...todo, completed: !todo.completed };
-  const response = await api.put(`/api/todos/${id}`, updatedTodo);
+  const response = await api.put(`/todos/${id}`, updatedTodo);
   return response.data;
 };
